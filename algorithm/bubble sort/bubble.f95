8f4342c7d001c5dd9b469d8f702a5152a69f24d4
yimang <lucasbbtung@gmail.com>
2024-05-16 20:34:27 +0800

    edit

--- algorithm/bubble sort/bubble.f95
+++ algorithm/bubble sort/bubble.f95
@@ -1,7 +1,11 @@
 program bubble
 
-    integer :: i, j, m, sort_num
+    implicit none
+    integer :: i, j, m, l, sort_num
     integer, allocatable :: a(:)
+    logical :: bool
+    bool = .true.
+
     write(*, '(A)', advance='no') "enter the number of numbers you want to sort:"
     read *, sort_num
     
@@ -17,15 +21,18 @@ program bubble
     m = size(a)
 
     do l = 1, m-1
+        if (.not. bool) exit !檢測上一輪是否有任何交換，沒有就代表已經排序完成可以提早結束程式
+        bool = .false.
         do i = 1, m-1
             if (a(i) > a(i+1)) then
                 j = a(i)
                 a(i) = a(i+1)
                 a(i+1) = j
+                bool = .true.
             end if
         end do
     end do
-
     print *, a
+
 
 end program bubble
